57e221f32f9731afabe3e61f00ef7a1f71997c8e
PriyankaRoteSuccessive <priyankarote@successive.tech>
2022-02-09 15:37:33 +0530

    Task Completed Counter

--- src/component/timer20.js
+++ src/component/timer20.js
@@ -1,73 +1,36 @@
 import React, { useState, useEffect } from 'react';
 const Timer20 = () => {
-    const [counter, setCounter] = useState(0);
-      const Update = () => {
-        let counter =0;
-        const interval = setInterval(() =>{
-          if(counter < 20){
-        counter = counter + 1;}
-          else if ( counter = 18){
-             counter=0
-              clearInterval(interval); 
-          
+  const [counter, setCounter] = useState(0);
+  const Increment = () => {
+    let FirstCount = counter;
+    let nextCount = true;
+    const interval = setInterval(() => {
+      console.log(FirstCount);
+      if (nextCount) {
+        if (FirstCount !== 20) {
+          FirstCount = FirstCount + 1;
+        } else {
+          nextCount = false;
+          FirstCount = FirstCount - 1;
         }
-          setCounter(counter);
-      },  1000)
-    }
-    useEffect(() => {
-      Update();
-    },[])
-      
-    return (
-      <>
-      <h1> Counter : { counter }</h1>
-      </>
-    ) 
-  };
+      } else if (!nextCount) {
+        if (FirstCount != 17) {
+          FirstCount = FirstCount - 1;
+        } else {
+          FirstCount = 0;
+          clearInterval(interval);
+        }
+      }
+      setCounter(FirstCount);
+    }, 1000)
+  }
+  useEffect(() => {
+    Increment();
+  }, [])
+  return (
+    <>
+      <h1>Counter: {counter} </h1>
+    </>
+  )
+}
 export default Timer20;
-// import React, { useState, useEffect } from 'react';
-// const Timer20 = () => {
-//     const [counter, setCounter] = useState(0);
-//     const [flag, setFlag] = useState("0");
-//       const Update = () => {
-//         let counter =0;
-
-//         const interval = setInterval(() =>{
-//             console.log(flag)
-//            if(flag == "0"){
-//             if(counter < 20){
-//                 counter = counter + 1;
-//                   }
-//                   else {
-//                       setFlag({flag:"1"});
-//                       console.log("false")
-//                   }
-//           setCounter(counter);
-//            }
-//            else{
-//             if(counter > 0){
-//                 counter = counter - 1;
-//                 console.log(flag)
-//                   }
-//                   else {
-                   
-//                       clearInterval(interval); 
-                 
-//                       console.log(flag)
-//                   }
-//           setCounter(counter);
-//            }
-//       },  1000)
-
-//     }
-//     useEffect(() => {
-//       Update();
-//     },[])
-      
-//     return (
-//       <>
-//       <h1> Counter : {counter}</h1>
-//       </>
-//     ) 
-//   };
-// export default Timer20;
